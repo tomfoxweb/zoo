0d8c260a69bacf8e389f83fc52a8b13fcaff54e0
Tom <tomwansww@gmail.com>
2021-09-24 22:37:44 +0500

    Giraffe

---
+++ giraffe.js
@@ -0,0 +1,5 @@
+module.exports = class Giraffe {
+    constructor() {
+        console.log("Melman");
+    }
+}
--- zoo.js
+++ zoo.js
@@ -1,6 +1,8 @@
 const Zebra = require("./zebra.js");
 const Hippo = require("./hippo.js");
+const Giraffe = require("./giraffe.js");
 
 console.log("Welcome to Zoo");
 const zebra = new Zebra();
 const hippo = new Hippo();
+const giraffe = new Giraffe();
