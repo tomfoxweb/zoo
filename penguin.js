5ea5ac2b77111e436df14523876a0566986c4f90
Tom <tomwansww@gmail.com>
2021-09-26 16:35:51 +0500

    Pingwin

---
+++ penguin.js
@@ -0,0 +1,5 @@
+module.exports = class Penguin {
+    constructor() {
+        console.log("Skipper");
+    }
+}
--- zoo.js
+++ zoo.js
@@ -1,6 +1,8 @@
 const Zebra = require("./zebra.js");
 const Hippo = require("./hippo.js");
+const Penguin = require("./penguin.js");
 
 console.log("Welcome to Zoo");
 const zebra = new Zebra();
 const hippo = new Hippo();
+const penguin = new Penguin();
